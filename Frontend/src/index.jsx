b9958fb8e23e92ce85bb666f6af219f3af2c39f0
Abhishek Gaikwad <87597373+ABHI19092002@users.noreply.github.com>
2023-07-15 09:55:27 +0530

    Update and rename index.js to index.jsx

---
+++ Frontend/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components//App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
+);
+
